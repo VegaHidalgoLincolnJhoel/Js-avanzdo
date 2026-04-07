bb986de532e03f9886d5d53d13084b05fd2b839e
VegaHidalgoLincolnJhoel <u23304682@utp.edu.pe>
2026-04-07 18:19:23 -0500

    avance

---
+++ Ejercicios_semana_2/index.js
@@ -0,0 +1,128 @@
+function sumar(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function operaciones(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    let resta = n1 - n2;
+    let multiplicacion = n1 * n2;
+    let division = n1 / n2;
+    document.getElementById("resultado").textContent = "Las operaciones basicas de los 2 valores son: "+ "\nSuma: " + suma + "\nResta: " + resta + "\nMultiplicacion: " + multiplicacion + "\nDivision: " + division;
+}
+
+function modulo(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let modulo = n1 % n2;
+    document.getElementById("resultado").textContent = "El modulo de la division es: \n"+ modulo;
+}
+
+function potencia(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let potencia = Math.pow(n1,n2);
+    document.getElementById("resultado").textContent = "La potencia es: \n" + potencia;
+}
+
+function subeybaja(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function IEEE754(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function redondeo(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function absolutoyraiz(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function bigInt(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function compaLogica(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function operadorOR(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function desplIzquierda(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function desplDerecha(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function SpreadArrays(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function SpreadConElementos(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function SpreadConObjetos(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function funDeclarada(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
+
+function Calculadora(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let suma = n1 + n2;
+    document.getElementById("resultado").textContent = "La suma de los 2 valores es: " + suma;
+}
